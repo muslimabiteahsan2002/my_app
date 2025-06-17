d3cbf4e42daefe0a88590a6322ed47cf863db029
MUSLIMA <muslimabintea@gmail.com>
2025-06-17 20:35:40 +0600

    rectangle

--- app/src/main/java/com/muslima/myrac/MainActivity.kt
+++ app/src/main/java/com/muslima/myrac/MainActivity.kt
@@ -10,5 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.areaBtn.setOnClickListener {
+            val lengthStr = binding.lengthEt.text.toString().trim()
+            val weightStr = binding.weightEt.text.toString().trim()
+            if (lengthStr.isEmpty()) {
+                binding.lengthEt.error = "Enter the length"
+            } else if (weightStr.isEmpty()) {
+                binding.weightEt.error = "Enter the weight"
+            } else {
+                val length = lengthStr.toInt()
+                val weight = weightStr.toInt()
+                rectangleArea(length, weight)
+            }
+        }
+    }
+
+    private fun rectangleArea(length: Int, weight: Int) {
+        val area = length * weight
+        binding.areaTv.text = "The area of rectangle is $area M^2"
     }
 }
