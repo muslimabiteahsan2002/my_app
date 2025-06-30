5077cc930f01aa1d162da75fb53a909907c76701
MUSLIMA <muslimabintea@gmail.com>
2025-06-30 23:36:59 +0600

    webview

--- app/src/main/java/com/muslima/myrac/MainActivity.kt
+++ app/src/main/java/com/muslima/myrac/MainActivity.kt
@@ -1,14 +1,48 @@
 package com.muslima.myrac
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.muslima.myrac.databinding.ActivityMainBinding
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.web.settings.javaScriptEnabled = true
+        binding.web.loadUrl("https://www.daraz.com.bd/#?")
+        binding.web.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                binding.pro.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                binding.pro.visibility = View.GONE
+            }
+        }
+        binding.web.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                super.onProgressChanged(view, newProgress)
+                binding.pro.progress = newProgress
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.web.canGoBack()) {
+            binding.web.goBack()
+        } else {
+            super.onBackPressed()
+        }
+
     }
 }
